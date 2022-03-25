a3a19ac57328e945796e8c4f542e02f4d89b11e9
Jon Aschberg <jon@aschberg.dk>
2022-03-25 22:33:07 +0100

    Add cool scene

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UI : MonoBehaviour
+{
+	[SerializeField] private UIDocument _uiDocument;
+
+	private void Start()
+	{
+		// _uiDocument.
+	}
+}
