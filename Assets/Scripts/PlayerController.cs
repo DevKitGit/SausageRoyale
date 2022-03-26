8a8068d9979b8b52f8ca1c2f3eefac77f7b139ca
Kit <me@devkit.dk>
2022-03-26 11:14:10 +0100

    Functioning events & Rigidbody input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,65 +1,105 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private InputActionAsset _asset;
     [SerializeField] private Rigidbody startRb;
     [SerializeField] private Rigidbody endRb;
     [SerializeField] private float moveForceScale = 1f;
-    private SausageInputActions _sausageInputActions;
-    private bool _inputActive;
-    // Start is called before the first frame update
+    private PlayerInput _playerInput;
+    private List<int> _inputIDs;
+
+    private Vector3 startForceVector = Vector3.zero;
+    private Vector3 endForceVector = Vector3.zero;
+    private bool applyStartForce = false;
+    private bool applyEndForce = false;
+    
+    public PlayerInput.ActionEvent
+        UiNavigate,
+        UiSelect,
+        UiBack,
+        PlayerJump,
+        PlayerMoveA,
+        PlayerMoveB,
+        PlayerGrabA,
+        PlayerGrabB,
+        PlayerPause;
+    
     void Start()
     {
-        _inputActive = false;
-        _sausageInputActions = new SausageInputActions();
-    }
-
-    private void AssignSausage(Rigidbody start, Rigidbody end)
-    {
-        _inputActive = true;
-        startRb = start;
-        endRb = end;
-    }
-
-    private void SetInputActive(bool inputActive)
-    {
-        _inputActive = inputActive;
-        if (inputActive)
-        {
-            _sausageInputActions.Player.MoveA.performed += MoveStartRB;
-            _sausageInputActions.Player.MoveB.performed += MoveEndRB;
-            _sausageInputActions.Player.Jump.performed += Jump;
-            return;
-        }
-        _sausageInputActions.Player.MoveA.performed -= MoveStartRB;
-        _sausageInputActions.Player.MoveB.performed -= MoveEndRB;
-        _sausageInputActions.Player.Jump.performed -= Jump;
-    }
-    private void Jump(InputAction.CallbackContext callbackContext)
-    {
-        //Future do stuff
-        Debug.Log("jump");
-    }
-
-    private void OnDisable()
-    {
-        SetInputActive(false);
-    }
-
-    private void MoveStartRB(InputAction.CallbackContext callbackContext)
-    {
-        var moveDirection = callbackContext.ReadValue<Vector2>() * moveForceScale;
-        startRb.AddForce(new Vector3(moveDirection.x, 0, moveDirection.y));
+        _playerInput = GetComponent<PlayerInput>();
+        SetupEvents();
     }
     
-    private void MoveEndRB(InputAction.CallbackContext callbackContext)
+    private void FixedUpdate()
     {
-        var moveDirection = callbackContext.ReadValue<Vector2>() * moveForceScale;
-        endRb.AddForce(new Vector3(moveDirection.x, 0, moveDirection.y));
+        if (applyStartForce)
+        {
+            startRb.AddForce(new(startForceVector.x*moveForceScale, 0, startForceVector.y*moveForceScale));
+        }
+        
+        if (applyEndForce)
+        {
+            endRb.AddForce(new(endForceVector.x*moveForceScale, 0, endForceVector.y*moveForceScale));
+        }
     }
-    // Update is called once per frame
+    
+    void SetupEvents()
+    {
+        var actionEvent = _playerInput.actionEvents;
+        UiNavigate = actionEvent.First(e => e.actionName.Contains("UI/Navigate"));
+        UiSelect = actionEvent.First(e => e.actionName.Contains("UI/Select"));
+        UiBack = actionEvent.First(e => e.actionName.Contains("UI/Back"));
+        PlayerJump = actionEvent.First(e => e.actionName.Contains("Player/Jump"));
+        PlayerMoveA = actionEvent.First(e => e.actionName.Contains("Player/MoveA"));
+        PlayerMoveB = actionEvent.First(e => e.actionName.Contains("Player/MoveB"));
+        PlayerGrabA = actionEvent.First(e => e.actionName.Contains("Player/GrabA"));
+        PlayerGrabB = actionEvent.First(e => e.actionName.Contains("Player/GrabB"));
+        PlayerPause = actionEvent.First(e => e.actionName.Contains("Player/Pause"));
+        PlayerJump.AddListener(Jump);
+        PlayerMoveA.AddListener(MoveStartRb);
+        PlayerMoveB.AddListener(MoveEndRb);
+        PlayerPause.AddListener(SetActionMap);
+        
+    }
+
+    private void SetActionMap(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+            _playerInput.SwitchCurrentActionMap("UI");
+        }
+    }
+    
+    void MoveStartRb(InputAction.CallbackContext callbackContext)
+    {
+        applyStartForce = callbackContext.performed;
+        if (callbackContext.performed)
+        {
+            startForceVector = callbackContext.ReadValue<Vector2>();
+        }
+    }
+    
+    void MoveEndRb(InputAction.CallbackContext callbackContext)
+    {
+        applyEndForce = callbackContext.performed;
+        if (callbackContext.performed)
+        {
+            endForceVector = callbackContext.ReadValue<Vector2>();
+        }
+    }
+    void Jump(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+
+        }
+    }
+
 
 }
