14ea764ea8873287ae26129d90046e36f66aa9de
Kit <me@devkit.dk>
2022-03-26 15:20:00 +0100

    Moved out sausage interactions into separate script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private InputActionAsset _asset;
-    [SerializeField] private Rigidbody startRb;
-    [SerializeField] private Rigidbody endRb;
-    [SerializeField] private float moveForceScale = 1f;
-    private PlayerInput _playerInput;
-    private List<int> _inputIDs;
+    public PlayerInput m_PlayerInput { get; private set; }
 
-    private Vector3 startForceVector = Vector3.zero;
-    private Vector3 endForceVector = Vector3.zero;
-    private bool applyStartForce = false;
-    private bool applyEndForce = false;
     public PlayerInput.ActionEvent
         UiNavigate,
         UISubmit,
@@ -31,27 +22,13 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
-        _playerInput = GetComponent<PlayerInput>();
+        m_PlayerInput = GetComponent<PlayerInput>();
         SetupEvents();
-    }
-    
-    private void FixedUpdate()
-    {
-        if (_playerInput.currentActionMap.name != "Player") return;
-        if (applyStartForce)
-        {
-            startRb.AddForce(new(startForceVector.x*moveForceScale, 0, startForceVector.y*moveForceScale));
-        }
-        
-        if (applyEndForce)
-        {
-            endRb.AddForce(new(endForceVector.x*moveForceScale, 0, endForceVector.y*moveForceScale));
-        }
     }
     
     void SetupEvents()
     {
-        var actionEvents = _playerInput.actionEvents;
+        var actionEvents = m_PlayerInput.actionEvents;
         UiNavigate = actionEvents.First(e => e.actionName.Contains("UI/Navigate"));
         UISubmit = actionEvents.First(e => e.actionName.Contains("UI/Submit"));
         UiCancel = actionEvents.First(e => e.actionName.Contains("UI/Cancel"));
@@ -61,42 +38,5 @@ public class PlayerController : MonoBehaviour
         PlayerGrabA = actionEvents.First(e => e.actionName.Contains("Player/GrabA"));
         PlayerGrabB = actionEvents.First(e => e.actionName.Contains("Player/GrabB"));
         PlayerPause = actionEvents.First(e => e.actionName.Contains("Player/Pause"));
-        PlayerJump.AddListener(Jump);
-        PlayerMoveA.AddListener(MoveStartRb);
-        PlayerMoveB.AddListener(MoveEndRb);
-        PlayerPause.AddListener(EnableUIActionMap);
-    }
-    
-    private void EnableUIActionMap(InputAction.CallbackContext callbackContext)
-    {
-        if (callbackContext.performed)
-        {
-            _playerInput.SwitchCurrentActionMap("UI");
-        }
-    }
-    
-    void MoveStartRb(InputAction.CallbackContext callbackContext)
-    {
-        applyStartForce = callbackContext.performed;
-        if (callbackContext.performed)
-        {
-            startForceVector = callbackContext.ReadValue<Vector2>();
-        }
-    }
-    
-    void MoveEndRb(InputAction.CallbackContext callbackContext)
-    {
-        applyEndForce = callbackContext.performed;
-        if (callbackContext.performed)
-        {
-            endForceVector = callbackContext.ReadValue<Vector2>();
-        }
-    }
-    void Jump(InputAction.CallbackContext callbackContext)
-    {
-        if (callbackContext.performed)
-        {
-
-        }
     }
 }
