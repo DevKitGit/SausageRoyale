975a05f1af6226a6eb9416c2e482f440580770af
Kit <me@devkit.dk>
2022-03-26 21:41:21 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Hotspot.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hotspot : MonoBehaviour
+{
+    public SphereCollider sphereCollider;
+    [SerializeField] private float rotationSpeed;
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var parent = other.transform.parent;
+        SausageController sausageController = null;
+        if (parent != null && parent.TryGetComponent(out sausageController))
+        {
+            sausageController.inHotspot = true;
+        }
+    }
+}
