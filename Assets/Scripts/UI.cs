9416de6fe1bd9e37eaf2517252fb73f6c4041942
Jon Aschberg <jon@aschberg.dk>
2022-03-26 16:18:28 +0100

    Unify UI

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,12 +1,77 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class UI : MonoBehaviour
 {
-	[SerializeField] private UIDocument _uiDocument;
+	private VisualElement _root;
 
-	private void Start()
+	private VisualElement _mainMenu;
+	private VisualElement _options;
+	private VisualElement _characterSelect;
+
+	private Stack<VisualElement> _history = new Stack<VisualElement>();
+	private VisualElement _navigation;
+
+	private void Back()
 	{
-		// _uiDocument.
+		if (_history.Count <= 1)
+		{
+			return;
+		}
+
+		var last = _history.Pop();
+		last.Display(false);
+
+		var current = _history.Peek();
+		current.Display(true);
+		
+		_navigation?.Display(_history.Count > 1);
+		current.GetFirstOfType<Button>()?.Focus();
 	}
+
+	private void NavigateTo(VisualElement menu)
+	{
+		if (_history.Count == 0)
+		{
+			_navigation?.Display(false);
+			_history.Push(menu);
+			return;
+		}
+
+		var last = _history.Peek();
+		last.Display(false);
+		_history.Push(menu);
+		menu.Display(true);
+		_navigation?.Display(true);
+		menu.GetFirstOfType<Button>()?.Focus();
+	}
+	
+	private void Awake()
+	{
+		_root = GetComponent<UIDocument>().rootVisualElement;
+		_mainMenu = _root.Q("main-menu");
+		_options = _root.Q("options-menu");
+		_characterSelect = _root.Q("character-select-menu");
+
+		_mainMenu.Display(true);
+		_options.Display(false);
+		_characterSelect.Display(false);
+
+		_navigation = _root.Q("navigation");
+		var navigationBack = _root.Q<Button>("back");
+		navigationBack.BindValue(Back);
+		
+		var optionsButton = _mainMenu.Q<Button>("options");
+		optionsButton.BindValue(() => NavigateTo(_options));
+		var newGameButton = _mainMenu.Q<Button>("newgame");
+		newGameButton.BindValue(() => NavigateTo(_characterSelect));
+		var exitButton = _mainMenu.Q<Button>("exit");
+		exitButton.BindValue(Application.Quit);
+
+
+		NavigateTo(_mainMenu);
+	}
+
 }
---
+++ Assets/Scripts/UIExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+public static class UIExtensions
+{
+	public static void Display(this VisualElement element, bool shouldDisplay)
+	{
+		if (element == null)
+		{
+			return;
+		}
+		
+		element.style.display = shouldDisplay ? DisplayStyle.Flex : DisplayStyle.None;
+	}
+
+	public static void Replace(this VisualElement root, VisualElement oldElement, VisualElement newElement)
+	{
+		int index = root.IndexOf(oldElement);
+		root.Insert(index, newElement);
+		root.Remove(oldElement);
+	}
+
+
+	public static void BindValue(this Button button, Action action) => button.RegisterCallback<ClickEvent>(_ => action?.Invoke());
+	
+}
